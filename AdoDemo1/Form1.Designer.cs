e08826c4bbe264d6fdd33ad0b617ac7978723ccb
NakkaAnilkumar <anilsunny1999@gmail.com>
2023-11-09 14:55:11 +0530

    Add project files.

---
+++ AdoDemo1/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace AdoDemo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDeptno = new System.Windows.Forms.TextBox();
+            this.txtdeptname = new System.Windows.Forms.TextBox();
+            this.txtdeptloc = new System.Windows.Forms.TextBox();
+            this.dgvDeptData = new System.Windows.Forms.DataGridView();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnShowAll = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDeptData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Deptno";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter DeptName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Enter Dept Location";
+            // 
+            // txtDeptno
+            // 
+            this.txtDeptno.Location = new System.Drawing.Point(217, 83);
+            this.txtDeptno.Name = "txtDeptno";
+            this.txtDeptno.Size = new System.Drawing.Size(100, 22);
+            this.txtDeptno.TabIndex = 3;
+            // 
+            // txtdeptname
+            // 
+            this.txtdeptname.Location = new System.Drawing.Point(217, 123);
+            this.txtdeptname.Name = "txtdeptname";
+            this.txtdeptname.Size = new System.Drawing.Size(100, 22);
+            this.txtdeptname.TabIndex = 4;
+            // 
+            // txtdeptloc
+            // 
+            this.txtdeptloc.Location = new System.Drawing.Point(217, 165);
+            this.txtdeptloc.Name = "txtdeptloc";
+            this.txtdeptloc.Size = new System.Drawing.Size(100, 22);
+            this.txtdeptloc.TabIndex = 5;
+            // 
+            // dgvDeptData
+            // 
+            this.dgvDeptData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDeptData.Location = new System.Drawing.Point(402, 48);
+            this.dgvDeptData.Name = "dgvDeptData";
+            this.dgvDeptData.RowHeadersWidth = 51;
+            this.dgvDeptData.RowTemplate.Height = 24;
+            this.dgvDeptData.Size = new System.Drawing.Size(368, 208);
+            this.dgvDeptData.TabIndex = 6;
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(91, 318);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(82, 44);
+            this.btnadd.TabIndex = 7;
+            this.btnadd.Text = "Add New Record";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(217, 318);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(84, 44);
+            this.btnEdit.TabIndex = 8;
+            this.btnEdit.Text = "Save Data";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(355, 318);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(81, 44);
+            this.btnFind.TabIndex = 9;
+            this.btnFind.Text = "Find Record";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // btnShowAll
+            // 
+            this.btnShowAll.Location = new System.Drawing.Point(604, 318);
+            this.btnShowAll.Name = "btnShowAll";
+            this.btnShowAll.Size = new System.Drawing.Size(147, 44);
+            this.btnShowAll.TabIndex = 10;
+            this.btnShowAll.Text = "Display All Records";
+            this.btnShowAll.UseVisualStyleBackColor = true;
+            this.btnShowAll.Click += new System.EventHandler(this.btnShowAll_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(486, 318);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 44);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Delete Record";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(486, 381);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(51, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(719, 411);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dept Details";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnShowAll);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.dgvDeptData);
+            this.Controls.Add(this.txtdeptloc);
+            this.Controls.Add(this.txtdeptname);
+            this.Controls.Add(this.txtDeptno);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDeptData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDeptno;
+        private System.Windows.Forms.TextBox txtdeptname;
+        private System.Windows.Forms.TextBox txtdeptloc;
+        private System.Windows.Forms.DataGridView dgvDeptData;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button btnShowAll;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ AdoDemo1/Form1.cs
@@ -0,0 +1,185 @@
+﻿using ADODemo1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoDemo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShowAll_Click(object sender, EventArgs e)
+        {
+            //ConfigurationManager.ConnectionStrings["projectcnString"].ConnectionString
+            SqlConnection cn = new SqlConnection("Data Source=SUNNYLAPPY\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+
+            try
+            {
+
+                SqlCommand cmd = new SqlCommand("select * from [dbo].ShowDeptDemo()", cn);
+                //Command to execute requires a open connection
+                cn.Open();
+                //select query in the command use executeReader to execute the command
+                SqlDataReader dr = cmd.ExecuteReader();
+                //dr.Read();//one row
+                //dr.Read();//second row
+                //HasRows is a  property whihc return bool to tell whether the tatble has rows or the table is empty
+                List<Dept> Deptlist = new List<Dept>();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        Dept d = new Dept();
+                        d.Deptno = Convert.ToInt32(dr["deptno"]);
+                        d.Deptname = dr["dname"].ToString();
+                        d.DeptLoc = dr["loc"].ToString();
+                        Deptlist.Add(d);
+                    }
+                }
+
+                dgvDeptData.DataSource = Deptlist;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+                cn.Dispose();
+            }
+
+
+
+
+
+
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            //System.Configuration(Right Click References=----Add Reference----Assembly...)
+            SqlConnection cn = new SqlConnection("Data Source=SUNNYLAPPY\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+
+            SqlCommand cmd = new SqlCommand("insert into department values(@deptname,@location)", cn);
+            cmd.Parameters.AddWithValue("@deptname", txtdeptname.Text);
+            cmd.Parameters.AddWithValue("@location", txtdeptloc.Text);
+            cn.Open();
+            //Insert/Update/Delete --cmd .ExecuteNonQuery()
+            cmd.ExecuteNonQuery();
+            cn.Close();
+            cn.Dispose();
+            MessageBox.Show("Successfully Completed");
+            btnShowAll_Click(sender, e);
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection("Data Source=SUNNYLAPPY\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("select * from department where deptno=" + Convert.ToInt32(txtDeptno.Text), cn);
+            cn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                dr.Read();
+                Dept d = new Dept();
+                d.Deptno = Convert.ToInt32(dr["deptno"]);
+                d.Deptname = dr["dname"].ToString();
+                d.DeptLoc = dr["loc"].ToString();
+
+                txtdeptname.Text = d.Deptname;
+                txtdeptloc.Text = d.DeptLoc;
+
+                //txtdeptname.Text= dr["dname"].ToString();
+                //txtdeptloc.Text = dr["loc"].ToString();
+
+
+
+                MessageBox.Show("Record found successfully");
+            }
+
+
+            cn.Close();
+            cn.Dispose();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection("Data Source=SUNNYLAPPY\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True");
+
+            SqlCommand cmd = new SqlCommand("UpdateDept", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@p_deptno", Convert.ToInt32(txtDeptno.Text));
+            cmd.Parameters.AddWithValue("@p_dname", txtdeptname.Text.Trim());
+            cmd.Parameters.AddWithValue("@p_loc", txtdeptloc.Text.Trim());
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            cn.Close();
+            cn.Dispose();
+            MessageBox.Show("Updated successfully");
+            btnShowAll_Click(sender, e);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // Create a SqlConnection to the database
+            using (SqlConnection cn = new SqlConnection("Data Source=SUNNYLAPPY\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True"))
+            {
+                try
+                {
+                    // Create a SqlCommand to execute the delete query
+                    using (SqlCommand cmd = new SqlCommand("DELETE FROM department WHERE deptno = @deptno", cn))
+                    {
+                        // Add the parameter for deptno
+                        cmd.Parameters.AddWithValue("@deptno", Convert.ToInt32(txtDeptno.Text));
+
+                        // Open the connection
+                        cn.Open();
+
+                        // Execute the delete query
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Record deleted successfully");
+                            // Clear text fields after deletion
+                            txtdeptname.Text = "";
+                            txtdeptloc.Text = "";
+                        }
+                        else
+                        {
+                            MessageBox.Show("No record found with the specified deptno");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+
+            // Refresh the data grid view by calling the ShowAll_Click event
+            btnShowAll_Click(sender, e);
+        }
+    }
+}
